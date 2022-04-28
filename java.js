4c2484ea5f23e2c49a130e0dbbbf4ee3899cc2ad
LUISX-X <103231487+LUISX-X@users.noreply.github.com>
2022-04-28 18:00:07 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+
+let menu = document.querySelector('#menu');
+let menu_bar = document.querySelector('#menu-bar');
+    
+  menu_bar.addEventrestobarer('click', function() {
+    menu.classList.toggle("menu-toggle");
+    
+  });
+
